53b3cf433f031a231684c810d0ad66f38be7dc9c
Chris Kruger <chris.kruger@krugerheavyindustries.com>
2016-11-07 10:10:58 +0800

    start of khi tap

---
+++ blazer.rb
@@ -0,0 +1,35 @@
+class Blazer < Formula
+  desc "Backblaze B2 command line client"
+  homepage "https://github.com/KrugerHeavyIndustries/blazer"
+  url "https://github.com/KrugerHeavyIndustries/blazer/archive/0.2.7.tar.gz"
+  version "0.2.7"
+  sha256 "bafde5f9ba0ecf0282026a8309e28d3185686379290044b42649aadb54f57827"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on 'restclient-cpp'
+  depends_on 'openssl'
+  depends_on 'jansson'
+
+  def install
+    system "autoreconf", "--install", "--force"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test blazer`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
