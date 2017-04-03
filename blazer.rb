f93b2d9673d1f658b0263ef1ef1c765ac979f27b
Chris Kruger <chris.kruger@mokosocialmedia.com>
2017-04-03 14:37:02 +0800

    properly updated sha256 for blazer formula

--- blazer.rb
+++ blazer.rb
@@ -3,7 +3,7 @@ class Blazer < Formula
   homepage "https://github.com/KrugerHeavyIndustries/blazer"
   url "https://github.com/KrugerHeavyIndustries/blazer/archive/0.3.7.tar.gz"
   version "0.3.7"
-  sha256 "28a2d85324494983982b5c0a1c20a722fd73d23f"
+  sha256 "13a26971babc9391e5937f5b2d5e1a9d8a7281f82fceff7dbdd27cb7a562d5ea"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
