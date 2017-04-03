8b94390afcfcc3e179ed28a86133fa40987e4345
Chris Kruger <chris.kruger@mokosocialmedia.com>
2017-04-03 14:34:15 +0800

    updated blazer to 0.3.7

--- blazer.rb
+++ blazer.rb
@@ -2,8 +2,8 @@ class Blazer < Formula
   desc "Backblaze B2 command line client"
   homepage "https://github.com/KrugerHeavyIndustries/blazer"
   url "https://github.com/KrugerHeavyIndustries/blazer/archive/0.3.6.tar.gz"
-  version "0.3.6"
-  sha256 "275dffea310ebf487a12f66390abe1d17e937b892cbd12ea3d9547d9bba6c2f2"
+  version "0.3.7"
+  sha256 "28a2d85324494983982b5c0a1c20a722fd73d23f"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
