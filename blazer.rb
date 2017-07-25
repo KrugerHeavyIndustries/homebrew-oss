af354e761969a1885d55c3f495b6e421967cc5e0
Chris Kruger <chris.kruger@krugerheavyindustries.com>
2017-07-25 12:53:01 +0800

    bumped blazer to version 0.4.0

--- blazer.rb
+++ blazer.rb
@@ -1,9 +1,9 @@
 class Blazer < Formula
   desc "Backblaze B2 command line client"
   homepage "https://github.com/KrugerHeavyIndustries/blazer"
-  url "https://github.com/KrugerHeavyIndustries/blazer/archive/0.3.8.tar.gz"
-  version "0.3.8"
-  sha256 "8c40e242247ceb30c4f87bbffe868b552690c3813d8811a9dffd07d360cb008a"
+  url "https://github.com/KrugerHeavyIndustries/blazer/archive/0.4.0.tar.gz"
+  version "0.4.0"
+  sha256 "17ce3b6fed9ae50a4bdbd6c1570295f4b90ab10a675fba71fe49b43fb30946da"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
