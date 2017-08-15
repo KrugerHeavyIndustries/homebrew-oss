d302a37c82dbdb18bef943ceb4efe6537018ded6
Chris Kruger <chris.kruger@krugerheavyindustries.com>
2017-08-15 23:14:48 +0800

    blazer 0.4.1 and restclient-cpp 0.4.6

--- blazer.rb
+++ blazer.rb
@@ -1,9 +1,9 @@
 class Blazer < Formula
   desc "Backblaze B2 command line client"
   homepage "https://github.com/KrugerHeavyIndustries/blazer"
-  url "https://github.com/KrugerHeavyIndustries/blazer/archive/0.4.0.tar.gz"
-  version "0.4.0"
-  sha256 "17ce3b6fed9ae50a4bdbd6c1570295f4b90ab10a675fba71fe49b43fb30946da"
+  url "https://github.com/KrugerHeavyIndustries/blazer/archive/0.4.1.tar.gz"
+  version "0.4.1"
+  sha256 "0869f50eadab905a7279524537a711010189b296cb43a48a96761cb222071c02"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
--- restclient-cpp.rb
+++ restclient-cpp.rb
@@ -1,9 +1,9 @@
 class RestclientCpp < Formula
   desc "C++ client for making HTTP/REST requests"
   homepage "http://code.mrtazz.com/restclient-cpp/"
-  url "https://github.com/KrugerHeavyIndustries/restclient-cpp/archive/0.4.5.tar.gz"
-  version "0.4.5"
-  sha256 "89fc7c7e0d994ca45df3033efa57866e352928f77f49711832baa2169593a925"
+  url "https://github.com/KrugerHeavyIndustries/restclient-cpp/archive/0.4.6.tar.gz"
+  version "0.4.6"
+  sha256 "cce0a7f0e7ccd7e03e20344789d1eae86f8f3c827b159ff5e7326c2ef9e7854a"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
