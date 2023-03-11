04523574ae35cdd420ff375886183d4b7594c51c
Chris Kruger <montdidier@users.noreply.github.com>
2023-03-12 07:13:14 +0800

    update restclient-cpp

--- restclient-cpp.rb
+++ restclient-cpp.rb
@@ -1,9 +1,9 @@
 class RestclientCpp < Formula
   desc "C++ client for making HTTP/REST requests"
   homepage "http://code.mrtazz.com/restclient-cpp/"
-  url "https://github.com/KrugerHeavyIndustries/restclient-cpp/archive/0.4.6.tar.gz"
-  version "0.4.6"
-  sha256 "cce0a7f0e7ccd7e03e20344789d1eae86f8f3c827b159ff5e7326c2ef9e7854a"
+  url "https://github.com/KrugerHeavyIndustries/restclient-cpp/archive/0.5.2.tar.gz"
+  version "0.5.2"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
